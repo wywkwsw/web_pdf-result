8025ad596d138593375210d8ca19f00576017682
yang <1308380266@qq.com>
2021-01-05 15:38:57 +0800

    pdf导出代码

---
+++ src/components/pdfpage1.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="PdfPage1">
+        <button type="button" class="btn btn-primary" @click="pdfBtn">导出PDF</button>
+        <div id="pdfDom" style="padding:10px;background-color:#fff;">
+            <img alt="Vue logo" src="@/assets/logo.png">
+            <h1>Welcome to Your Vue.js App</h1>
+            <p v-for="(item,index) in 5" :key="index">{{item}}Welcome to Your Vue.js App</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import htmlToPdf from '@/utils/htmlToPdf'
+    export default {
+        name: "PdfPage",
+        data(){
+            return{
+                htmlTitle:'页面导出PDF文件名',
+            }
+        },
+        methods:{
+            pdfBtn(){
+                htmlToPdf.getPdf(this.htmlTitle);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/pdfpage2.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="PdfPage2">
+        <div class="no-print">
+            <button type="button" class="btn btn-primary" @click="pdfBtn">导出PDF</button>
+        </div>
+        <div id="pdfDom" style="padding:10px;background-color:#fff;">
+            <img alt="Vue logo" src="@/assets/logo.png">
+            <h1>Welcome to Your Vue.js App</h1>
+            <p v-for="(item,index) in 5" :key="index">{{item}}Welcome to Your Vue.js App</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PdfPage2",
+        methods:{
+            pdfBtn(){
+                window.print();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+/*保存时的样式*/
+/*
+了解更多可 百度CSS print样式
+*/
+@media print{
+    .no-print{
+        display: none;
+    }
+}
+/*打印页配置*/
+@page{
+    margin:60px 10px;
+}
+</style>
